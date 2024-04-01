4399d4671a9e4fbe4a06e69659d5e50bf173a880
polsito <lordgelsin26@gmail.com>
2024-04-01 13:08:18 -0500

    basics: 10 basics of health

--- bussines/openia-prompts/10 basics of health.ts
+++ bussines/openia-prompts/10 basics of health.ts
@@ -1,19 +1,72 @@
 /* 
-    take sun
-        supplement with vitamin D
-    avoid stress
-        parasimpatic mode - 8
-    intermitent fasting
-    sleep good
-    go to sauna
-    earthing
-    lowcarb foods
-    real food
+    healthy habits (time - expected results)
+        take sun
+        sleep good
+        circadian ritms
+        touch nature
+        duchas frias combined with sauna
+        intermitent fasting
+        reduce electromagnetic wives
+        avoid stress (to tiempo)
+            parasimpatic mode - 8
+            meditation
+        earthing
+    supplement with 
+        vitamin D
+        mangesium
+    nutrition
+        lowcarb foods
+        real food
+        disease foods
     training
-    reduce electromagnetic wives
-    circadian ritms
-    duchas frias combined with sauna
-    touch nature
-    mangesium
-    meditation
+    blod tests
+    doctors
+    
+    fisioterapy
+*/
+
+/* 
+    keys
+        bad diet
+        leaky gut
+        nutritional defficiencies
+        infections (fungus, parasites, bacteries, viruses)
+        intoxications (heavy metals, etc)
+
+*/
+
+/* 
+    leaky gut - hipotiroidism - estreñimiento - enfermedad autoinmune
+    leaky gut - estreñimiento, parasitos, sibo - alzheimer - enfermedad autoinmune
+    leaky gut - estreñimiento, parasitos - alzheimer - cancer
+
+    leaky gut - artrosis 
+
+*/
+
+/* 
+    * app modules
+        exams
+        mealplans
+        profile
+*/
+
+/* 
+
+    * Tables
+        users
+        mealplans
+        disease 
+            prompts
+
+*/
+
+/* 
+    * microservices
+        auth
+        users
+        mealplans
+        diseases
+        chatgpt
+        mealplan-pdfs
 */
