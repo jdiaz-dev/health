d93012573a2ca87d0687f96d7cb69f7f5f83c622
polsito <lordgelsin26@gmail.com>
2024-04-06 20:00:54 -0500

    fix: working with hypotyrodism

---
+++ bussines/openia-prompts/hyphotirodism.ts
@@ -0,0 +1,21 @@
+/* 
+    - consider tyrotropine to use medication or not
+
+*/
+
+/* 
+    solve problem
+        remove infecctions
+        reduce inflamation (void vegetarian foods)
+
+    nutrition
+        low carb diet
+        void high antinutrient foods
+        high protein foods
+        selenium
+        yodo (void excess of iodo)
+
+    important
+        iodine is the key component to produce thyroid hormones
+
+*/
