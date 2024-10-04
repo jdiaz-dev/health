4d0c06e99cb8d7685191f2c1cc3e5e4623459cad
polsito <lordgelsin26@gmail.com>
2024-10-04 17:07:28 -0500

    fix: help against the parasites

---
+++ bussines/videos/2 the parasites can destroy your life.ts
@@ -0,0 +1,33 @@
+/* 
+    - los parasites pueden estar provocando tu depresion
+    - los parasitos pueden provocando tu tiroiditis de hashimoto
+    - los parasitos pueden estar provocando tu fibromialgia
+    - los parasitos te pueden estar volviendo perezoso 
+    - los parasitos pueden estar provocando tu mal humor
+    - los parasitos pueden estar provocando tu fatiga
+    - los parasitos pueden estar provocando tu dolor de cabeza
+    - los parasitos pueden estar provocando tu dolor de estomago
+    - los parasitos pueden estar provocando tu dolor de espalda
+    - los parasitos pueden estar provocando tu dolor de rodilla
+    - los parasitos pueden estar provocando tu estreñimiento
+    - los parasitos pueden estar provocando que le desees a alguien la muerte
+    - los parasitos pueden estar disminuyendo tu rendimiento en el trabajo
+    - si estas parasitado, tu podrias estar destruyendo tu familia sin saberlo
+    - los parasitos podrian estar causandote enfermedades mas graves que la fibromialgia como el cancer o artritis reumatoide 
+
+    - he estado deprimido desde la adolescia y hace unos meses apenas que estoy superando la depresion
+    - crei que no podia superar a la chica que me gustaba porque estaba traumado, pero en realidad no tenia la capacidad de superarla porque estaba parasitado
+    - yo he estado estreñido por un poco mas de 3 años y no sabia que era debido a los parasitos
+
+
+    - mi teoria que es que creo que los parasitos son una herramienta del diablo, y debido a que estas parasitado podrias estar siendo una herramienta del diablo
+
+    - tienes que eliminar los parasitos si quieres empezar a mejorar tu vida
+    - antes de salir de parasitos, corrije tu fibromialgia
+    - entiende que son las crisis curativas,  que te podria ocurrir cuando empiezes a remover parasitos 
+    - toma jugos de zanahoria
+    - inicia una dieta vegana
+    - lee los protocolos del doctor ludwing johnson que tiene un monton de testimonios de pacientes curados de distintas enfermedades, muchos de ellos causados por parasitos 
+
+
+*/
