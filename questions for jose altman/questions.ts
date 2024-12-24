d90cb15baec3661971298438c2c33bd46a2dd9e0
polsito <lordgelsin26@gmail.com>
2024-12-24 06:43:03 -0500

    fix: saving questions

---
+++ questions for jose altman/questions.ts
@@ -0,0 +1,8 @@
+/* 
+    - it is possible to heal sida only breathing?
+    - it is possible to heal arthorisis only breathing and untoxifiying?
+    - we should not realize carnivore diet?
+    - there are some doctors that olive oil and coconut oil is bad to consume for this reason (....), we should consume it?
+
+
+*/
---
+++ video-ideas/personal development.ts
@@ -0,0 +1,16 @@
+/* 
+    * title for video : ?
+    * content
+      - chrisbeatcancer say the base of everything is the nutrition but jose altmna say that the base is the breathing
+
+
+    steps to realize 
+        - check the video where chris say that the base of the health is the nutritions
+        - check videos of jose altman where say the base of the health is the breathing
+        - check the bible, where say that the base of health is the breathing
+
+
+    - test start anise
+
+
+*/
