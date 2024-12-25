ce0349712901e657d6e144f13ba9d4ecf4f024a8
polsito <lordgelsin26@gmail.com>
2024-12-25 15:02:19 -0500

    detox: jose altman

--- questions for jose altman/questions.ts
+++ questions for jose altman/questions.ts
@@ -4,6 +4,13 @@
     - we should not realize carnivore diet?
     - there are some doctors that olive oil and coconut oil is bad to consume for this reason (....), we should consume it?
     - when we run at high velocities, we should breathe by the nose or by the mouth
+    - we ca grassfed meat?
+    - what foods recommend to win muscle mass
+    - jugos verdes
 
 
+    -> acelerado
+    9 dias de ayuno de frutas (papaya)
+    -sandias y melones deberiamos comerlos solos
+    - 9 dias solo jugos, junto a tierra diatomea, hierbas alcalinas amargas, silem host (trigo de la india)
 */
