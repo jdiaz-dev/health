2e9bf23ca400d56c3d65de1d8fd1744a6ab5b376
polsito <lordgelsin26@gmail.com>
2025-01-05 16:37:10 -0500

    fix: saving questions

--- questions for jose altman/questions.ts
+++ questions for jose altman/questions.ts
@@ -1,16 +1,43 @@
 /* 
-    - it is possible to heal sida only breathing?
-    - it is possible to heal arthorisis only breathing and untoxifiying?
-    - we should not realize carnivore diet?
-    - there are some doctors that olive oil and coconut oil is bad to consume for this reason (....), we should consume it?
-    - when we run at high velocities, we should breathe by the nose or by the mouth
-    - we ca grassfed meat?
-    - what foods recommend to win muscle mass
-    - jugos verdes
-
 
     -> acelerado
     9 dias de ayuno de frutas (papaya)
     -sandias y melones deberiamos comerlos solos
     - 9 dias solo jugos, junto a tierra diatomea, hierbas alcalinas amargas, silem host (trigo de la india)
+*/
+
+/* 
+    BREATHWORK
+        - during the gentile contractions of pelvic soil, it must be strong or weak, tu lo haces bien corto yo lo estoy haciendo bastante largo
+        - can I workout separated the 2 groups of inhanlations and exhalations?
+        - at moment to exhalate is normal to go quick at the end of the exhalation?
+        - must not exist estress?
+        - why I low weight when I do breathwork?
+        - during the first block, during the exhalations, How many time should I realize every exhalation (3 exhalations)? 
+
+*/
+
+/* 
+    DISEASES
+        - it is possible to heal hashimoto hipotiroidism, osteoarthrosis, CKD, diabetes type 1 with breathwork and detoxifing?
+        - it is possible to heal sida only breathing?
+
+
+*/
+
+/* 
+    - WORKOUT
+        - i must workout when I realize the breathwork?
+        - how to meet protein requirements if I use only the foods that you recommend?
+        - during breakfasting do you workout?
+        - when we run at high velocities, we should breathe by the nose or by the mouth
+
+*/
+
+
+/* 
+    NUTRICION
+        - we should not realize carnivore diet?
+        - there are some doctors that olive oil and coconut oil is bad to consume for this reason (....), we should consume it?
+
 */
