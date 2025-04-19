7083817a537eb98d0ac95615b78ce3b35bb7d1c8
polsito <lordgelsin26@gmail.com>
2025-04-18 21:21:16 -0500

    fix

--- accounts/accounts.ts
+++ accounts/accounts.ts
@@ -4,3 +4,9 @@
     personal english tiktok - jonathan.a.diaz.m@gmail.com
     personal english youtube - jonathanalejandrodm369@gmail.com
 */
+
+/* 
+    personal spanish instagram - lordgelsin26@gmail.com
+    personal spanish tiktok - software.athvio@gmail.com
+
+*/
