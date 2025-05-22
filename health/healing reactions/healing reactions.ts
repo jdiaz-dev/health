5633b27ee25601c156eb9d3403c94415b09c9a40
polsito <lordgelsin26@gmail.com>
2025-05-22 09:53:04 -0500

    fix

---
+++ health/healing reactions/healing reactions.ts
@@ -0,0 +1,21 @@
+/* 
+
+    * alleviate healing reactions
+        - comfrey ointment 
+        - comfrey oil (bone fracture, )
+        - Boswellia Carterii oil
+        - cbd oil! 
+        - PEA (Palmitoylethanolamide) 
+            — a naturally occurring fatty acid compound that’s been gaining attention for its pain-relieving and anti-inflammatory properties.
+            * What is PEA?
+            - Palmitoylethanolamide (PEA) is a lipid mediator naturally produced by the body in response to inflammation and pain.
+        - “morphine” bomb: Put frankincense, Copiaba, & balsam fir
+        - epsom salt and borax powder in the water
+        - castor oil 
+        - backing soda
+        - Essiac tea 
+        - Aloe Vera
+        - colloidal silver
+        - Rick Simpson oil
+        - Coffee enemas
+*/
