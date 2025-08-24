7c87d5b205a3c368ee9bfea970e83260c6d0ed22
polsito <lordgelsin26@gmail.com>
2025-08-23 20:26:58 -0500

    fix

--- accounts/accounts.ts
+++ accounts/accounts.ts
@@ -9,5 +9,16 @@
     personal spanish instagram - lordgelsin26@gmail.com
     personal spanish tiktok - software.athvio@gmail.com
     personal spanish youtube - lordgelsin27@gmail.com
+    personal spanish youtube (hotels) jona.diaz.m26@gmail.com
+
+*/
+
+/* 
+    test google 
+    marisol1.mov1@gmail.com
+    benjamin.perez.t6@gmail.com
+    ulucas581@gmail.com
+    pieroponce63@gmail.com
+
 
 */
---
+++ research/research.md
@@ -0,0 +1,7 @@
+# Research
+
+- Organic minerals tend to have higher bioavailability because the body more easily recognizes and absorbs mineral forms bound to organic molecules, similar to how minerals occur naturally in food. (https://www.researchgate.net/publication/393990035_Organic_Vs_Inorganic_Mineral_Supplements_in_Poultry_Availability_and_Physiological_Effects_Subject_Review)
+
+
+
+
