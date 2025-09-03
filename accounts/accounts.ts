fc13a9dd9918869a786c16efd315453ad7697d96
polsito <lordgelsin26@gmail.com>
2025-09-03 15:45:36 -0500

    fix

--- accounts/accounts.ts
+++ accounts/accounts.ts
@@ -1,17 +1,26 @@
 /* 
 
-    personal english instagram - jonathan.a.diaz.m@gmail.com
     personal english tiktok - jonathan.a.diaz.m@gmail.com
     personal english youtube - jonathanalejandrodm369@gmail.com
 */
 
 /* 
-    personal spanish instagram - lordgelsin26@gmail.com
     personal spanish tiktok - software.athvio@gmail.com
     personal spanish youtube - lordgelsin27@gmail.com
+    personal spanish instagram - lordgelsin27@gmail.com
     personal spanish youtube (hotels) jona.diaz.m26@gmail.com
 
 */
+
+/* 
+    suspended by me:  
+        personal spanish instagram - lordgelsin26@gmail.com  (i decied to use the other instagram because erika is there)
+        personal english instagram - jonathan.a.diaz.m@gmail.com
+
+
+
+*/
+
 
 /* 
     test google 
