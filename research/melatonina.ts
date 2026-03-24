3acf92770c14d33b9ac3d4fcb495f1b4978f6d18
jonathanDiaz <jonathan.a.diaz.m@gmail.com>
2026-03-23 20:18:47 -0500

    fix

---
+++ research/melatonina.ts
@@ -0,0 +1,20 @@
+/* 
+
+FUNCTIONES DE LA MELATONINA:
+
+* Reloj biológico
+    - Tiene propiedades antioxidantes, antiinflamatorias e inmunoestimulantes
+
+* Salud mental y cognitiva
+    - La evidencia clínica sugiere que puede contribuir a mejorar la calidad del descanso, regular el estado de ánimo, mitigar síntomas de ansiedad y proteger contra el deterioro cognitivo
+
+* Posible rol anticancerígeno
+    - Estudios epidemiológicos han evidenciado un efecto oncostático en diferentes tipos de tumores. En ensayos clínicos, la melatonina se mostró potencialmente efectiva como tratamiento coadyuvante del cáncer, reforzando los efectos terapéuticos y reduciendo los efectos secundarios de quimioterapia y radioterapia
+
+
+*/
+
+/* 
+    TERMINOLOGY:
+        oncostático: oncostático se refiere a la capacidad de una sustancia, como la melatonina, para inhibir, detener o frenar el crecimiento y la proliferación de células tumorales
+*/
